f340061f5015c9ff6a851f3904bde8315b7c6395
williamlu05 <william.lu.bjornestad@uma.es>
2026-05-05 07:58:41 +0200

    first start

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlu-bjor <wlu-bjor@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 22:47:04 by wlu-bjor          #+#    #+#             */
+/*   Updated: 2026/05/04 22:47:04 by wlu-bjor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF.H
+# define LIBFTPRINTF.H
+
+// # include "libft.h"
+# include <stdarg.h>
+
+int	ft_printf(char const *, ...);
+
+#endif
