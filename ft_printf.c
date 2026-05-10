be7483889950c07c2387c4cb4ed565e918d18cfe
William Lu Bjornestad <wlu-bjor@c1r5s3.42malaga.com>
2026-05-10 12:16:55 +0200

    Comments removed

--- ft_printf.c
+++ ft_printf.c
@@ -63,23 +63,3 @@ int	ft_printf(char const *format, ...)
 	va_end(args);
 	return (written);
 }
-
-/*
-PLANNING:
-
-printf returns the number of characters printed on screen
-- Any errors returns a negative value
-
-1. Call a function that finds the number of '%' found (maybe loop of ft_strchr),
-2. Initiate the args with va_start, with the number of '%' found
-3. Loop over the linked list, and pass the type depending on what was stored of "cspdiuxX%"
-4. Have the function write up until that instance of '%', 
-	and then write what was in the argument, how it is written varies on the type
-5. 
-
-Things to remember:
-- Freeing all of the allocated space used with the auxilliary linked list,
-	both in a correct or failed execution
-- You can use the comparisons with the original printf():
-	"La función se comparará con el printf() original para verificar su comportamiento."¡
-*/
